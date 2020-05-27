ebe6a780b8c65e73443834fd89717f64a4226fb7
POOJA-ANAND-KUMAR <49919979+POOJA-ANAND-KUMAR@users.noreply.github.com>
2020-05-27 00:23:41 -0700

    Create About.js

---
+++ ReactClub/guest/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import snorkell from "../../images/snorkl-index.png";
+
+
+function AboutContent()
+{
+// Use in JSX
+
+  let cont=<>
+<section id="Login_box">
+           <h2 className="myfont">About</h2>
+    <p>Welcome to my exciting About-page</p>
+      </section>
+    </>;
+       
+     
+    return cont;
+    
+    }
+export default AboutContent;
+ 
